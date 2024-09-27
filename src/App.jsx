164d698223df6628b9dc22339171f88a0f186e4c
Fithi Abraham <fabraham@macos-t765xv452y.interos.ai>
2024-09-27 17:06:24 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TableCell from "./components/TableCell"
+
+export default function App() {
+  return (
+   <div className="bg-black h-dvh w-full">
+<div className="p-10">
+  <p className="text-white text-[10px] leading-[18px] font-medium tracking-[0.4px] p-4 font-p">I'm caption</p>
+  <p className="text-white text-[12px] leading-[20px] tracking-[0.2px] p-4 font-p">I'm Paragraph 3</p>
+  <p className="text-white text-[14px] leading-[22px] tracking-[0.2px] p-4 font-p">I'm Paragraph 2</p>
+  <p className="text-white text-[16px] leading-[24px] tracking-[0.2px] p-4 font-p">I'm Paragraph 1</p>
+</div>
+   </div>
+  )
+}
+
---
+++ src/components/TableCell.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TableCell = () => {
+  return (
+    <div className='border h-3 w-3 bg-black'>
+      
+    </div>
+  )
+}
+
+export default TableCell
+
